74a6cf4b8b8048d9647856fcf37191067026b1fb
Rogif <135497831+Rogif@users.noreply.github.com>
2024-06-01 02:21:24 +0300

    Grid pixels work

--- sketch.js
+++ sketch.js
@@ -1,24 +1,37 @@
 let DRAW_COLOR = 'red';
 let BACKGROUND_COLOR = 'white';
-let CURRENT_COLOR = BACKGROUND_COLOR;
+let currentColors = [];
+let CANVAS_WIDTH = 400;
+let CANVAS_HEIGHT = 400;
+let PIXEL_RESOLUTION_WIDTH = 10;
+let PIXEL_RESOLUTION_HEIGHT = 10;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  currentColors = new Array(PIXEL_RESOLUTION_WIDTH).fill().map(() => new Array(PIXEL_RESOLUTION_HEIGHT).fill(BACKGROUND_COLOR));
   background(BACKGROUND_COLOR);
+  for (let i = 0; i < PIXEL_RESOLUTION_WIDTH; i++) {
+    for (let j = 0; j < PIXEL_RESOLUTION_HEIGHT; j++) {
+      rect(i * (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), j * (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT), (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT));
+    }
+  }
 }
 
 function draw() {
 
 }
 
-function mousePressed(event) {
-  if (CURRENT_COLOR === BACKGROUND_COLOR) {
-    CURRENT_COLOR = DRAW_COLOR;
+function mouseClicked(event) {
+  let pixelX = Math.floor(mouseX / (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH));
+  let pixelY = Math.floor(mouseY / (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT));
+  console.log(currentColors);
+  if (currentColors[pixelX][pixelY] === BACKGROUND_COLOR) {
+    currentColors[pixelX][pixelY] = DRAW_COLOR;
   }
   else {
-    CURRENT_COLOR = BACKGROUND_COLOR;
+    currentColors[pixelX][pixelY] = BACKGROUND_COLOR;
   }
-  fill(CURRENT_COLOR);
-  rect(0, 0, 50, 50);
-  console.log('mousePressed', event)
+
+  fill(currentColors[pixelX][pixelY]);
+  rect(pixelX * (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), pixelY * (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT), (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT));
 }
