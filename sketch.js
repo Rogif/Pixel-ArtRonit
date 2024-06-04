9a9067e2cb1cface4d4f21b6d4489cd59dd4d136
Rogif <135497831+Rogif@users.noreply.github.com>
2024-06-04 16:42:41 +0300

    change let to BackColor

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
 let DRAW_COLOR = 'red';
-let BACKGROUND_COLOR = 'white';
+let BackColor = 'white';
 let currentColors = [];
 let CANVAS_WIDTH = 400;
 let CANVAS_HEIGHT = 400;
 let PIXEL_RESOLUTION_WIDTH = 10;
 let PIXEL_RESOLUTION_HEIGHT = 10;
+let resetButton;
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  currentColors = new Array(PIXEL_RESOLUTION_WIDTH).fill().map(() => new Array(PIXEL_RESOLUTION_HEIGHT).fill(BACKGROUND_COLOR));
-  background(BACKGROUND_COLOR);
+  currentColors = new Array(PIXEL_RESOLUTION_WIDTH).fill().map(() => new Array(PIXEL_RESOLUTION_HEIGHT).fill(BackColor));
+  background(BackColor);
   for (let i = 0; i < PIXEL_RESOLUTION_WIDTH; i++) {
     for (let j = 0; j < PIXEL_RESOLUTION_HEIGHT; j++) {
       rect(i * (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), j * (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT), (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT));
     }
   }
+  // create reset button
+  resetButton = createButton('Reset');
+  resetButton.position(50, CANVAS_HEIGHT + 70);
+  resetButton.mousePressed(resetCanvas);
 }
 
 function draw() {
@@ -25,11 +30,11 @@ function mouseClicked(event) {
   let pixelX = Math.floor(mouseX / (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH));
   let pixelY = Math.floor(mouseY / (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT));
   console.log(currentColors);
-  if (currentColors[pixelX][pixelY] === BACKGROUND_COLOR) {
+  if (currentColors[pixelX][pixelY] === BackColor) {
     currentColors[pixelX][pixelY] = DRAW_COLOR;
   }
   else {
-    currentColors[pixelX][pixelY] = BACKGROUND_COLOR;
+    currentColors[pixelX][pixelY] = BackColor;
   }
 
   fill(currentColors[pixelX][pixelY]);
@@ -39,5 +44,15 @@ function mouseClicked(event) {
 function keyPressed() {
   if (key == 's') {
     saveCanvas('photo', 'jpg');
+  }
+}
+
+function resetCanvas() {
+  currentColors = new Array(PIXEL_RESOLUTION_WIDTH).fill().map(() => new Array(PIXEL_RESOLUTION_HEIGHT).fill(BackColor));
+  background;
+  for (let i = 0; i < PIXEL_RESOLUTION_WIDTH; i++) {
+    for (let j = 0; j < PIXEL_RESOLUTION_HEIGHT; j++) {
+      rect(i * (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), j * (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT), (CANVAS_WIDTH / PIXEL_RESOLUTION_WIDTH), (CANVAS_HEIGHT / PIXEL_RESOLUTION_HEIGHT));
+    }
   }
 }
